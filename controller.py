40bc44efa9ba5640a9e951b24ffaff9f803592d5
mominaaazam <mominaazam08@gmail.com>
2026-04-02 11:47:18 -0500

    Add support for splitting tip among multiple people

--- controller.py
+++ controller.py
@@ -8,6 +8,8 @@ Builder.load_file('view.kv')
 class TipCalculator(BoxLayout):
     bill_input = StringProperty()
     tip_output = StringProperty()
+    num_people_input = StringProperty()
+    split_output = StringProperty()
     
     __model: TipModel
 
@@ -19,8 +21,10 @@ class TipCalculator(BoxLayout):
         try:
             self.__model.bill_amount = float(self.bill_input)
             self.__model.tip_percent = float(tip_percent)
-            
+            self.__model.num_people = int(self.num_people_input)
+        
             self.tip_output = f"Tip Amount: ${self.__model.tip_amount:.2f}"
+            self.split_output = f"Per Person: ${self.__model.total_per_person:.2f}"
 
         except ValueError as ex:
             print("error")
